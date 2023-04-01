8c2b9d2bb58a03400fc8c9d5c28741b54b4b2b33
Trofimov Alexandr <kanavtov@yahoo.com>
2023-04-01 16:48:28 +0300

    Ulearn - Enumerable twice!

--- Analysis.cs
+++ Analysis.cs
@@ -19,24 +19,29 @@ namespace Delegates.PairsAnalysis
             }
         }
 
-        public static int MaxIndex<T>(this IEnumerable<T> collection, Func<T, double> process)
+        public static int MaxIndex<T>(this IEnumerable<T> collection)
         {
-            return collection.Select((a, index) => new { a, index }).Where(b => process(b.a) ==
-                collection.Select(c => process(c)).Max()).Select(d => d.index).First();
+            return collection.Select((a, index) => (a, index)).Max().index;
+        }
+
+        public static IEnumerable<double> Process(this IEnumerable<Tuple<DateTime, DateTime>> collection)
+        {
+            return collection.Select(a => (a.Item2 - a.Item1).TotalSeconds);
+        }
+
+        public static IEnumerable<int> Process(this IEnumerable<Tuple<int, int>> collection)
+        {
+            return collection.Select(a => (a.Item2 - a.Item1));
         }
 
         public static int FindMaxPeriodIndex(params DateTime[] data)
         {
-            Func<Tuple<DateTime, DateTime>, double> process = source => (source.Item2 - source.Item1).TotalSeconds;
-            var query = data.Pairs().MaxIndex(process);
-            return query;
+            return data.Pairs().Process().MaxIndex();
         }
 
         public static double FindAverageRelativeDifference(params double[] data)
         {
-            Func<Tuple<double, double>, double> process = source => (source.Item2 - source.Item1) / source.Item1;
-            var query = data.Pairs().Average(process);
-            return query;
+            return data.Pairs().Average(source => (source.Item2 - source.Item1) / source.Item1);
         }
     }
 }
